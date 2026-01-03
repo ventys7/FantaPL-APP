4cdfc7dd0a544f22f7a499f323ba9bd824d7e31a
Paolo Briamo <paolo@MacBookPro.station>
2026-01-03 11:58:00 +0100

    fix: Final Mobile Scroll (overscroll-none + h-dvh)

--- assets/ParticipantManagement-BrboDCAl.js
+++ assets/ParticipantManagement-BrboDCAl.js
@@ -1,4 +1,4 @@
-import{c as u,u as q,r as o,d as c,D as d,g as m,Q as H,j as e,h as O,P as V,f as B,I as A}from"./index-hiTsnC1T.js";import{L as K}from"./lock-CuI6fzFu.js";import{T as Q,C as F}from"./trash-2-B8UaokF4.js";/**
+import{c as u,u as q,r as o,d as c,D as d,g as m,Q as H,j as e,h as O,P as V,f as B,I as A}from"./index-D11w6Mer.js";import{L as K}from"./lock-D8JXJvVO.js";import{T as Q,C as F}from"./trash-2-BtSsIq01.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
