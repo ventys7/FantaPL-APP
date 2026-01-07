2cc4e3fc99f21fe05765d22e2bc79697861a08c8
ventys7 <224636084+ventys7@users.noreply.github.com>
2026-01-07 22:25:51 +0000

    Deploying to gh-pages from @ ventys7/FantaPL-APP@cdc7a707ac6e4797e293deacc0d33e46a1e1130d 🚀

--- assets/plus-DxAts5h7.js
+++ assets/plus-DxAts5h7.js
@@ -1,4 +1,4 @@
-import{c as s}from"./index-DLw58pc8.js";/**
+import{c as s}from"./index-BgNnxJZX.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
